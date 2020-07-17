5d47c34f07cc63eb80d88f608469e924b3bd96d7
Amaan Mahammad <60258136+amahammad132@users.noreply.github.com>
2020-07-16 20:13:23 -0700

    Switch to high resolution clock
    Clean up code
    
    Remove deprecated constants

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "useful_functions.h"
 
 #define YELLOW "\033[33m"
-#define BLUE "\033[34m"
 #define BOLD_RED "\033[1m\033[31m"
 #define GREEN "\033[92m"
 #define CYAN "\033[96m"
@@ -15,18 +14,8 @@
 
 using namespace std;
 
-// these are deprecated constants
-const int THREADS = 12;
-const int STARTING = 1;
-const int ENDING = 1000000;
-
 int main(int argc, char **argv) {
-#ifdef __INTEL_COMPILER
-    cout << BLUE << "intel compiler used" << RESET << endl;
-#elif defined(__GNUC__)
-    cout << BLUE << "gcc compiler used" << RESET << endl;
-#endif
-    auto start = chrono::system_clock::now();
+    auto starting_time = chrono::high_resolution_clock::now();
 
     int starting = 1;
     int ending = 1000000;
@@ -39,32 +28,16 @@ int main(int argc, char **argv) {
             stringstream arg;
             switch (argv[i][1]) {
                 case 'r':
-//                    arg << argv[i + 1];
-//                    arg >> starting;
-//                    stringstream arg2;
-//                    arg2 << argv[i + 2];
-//                    arg2 >> ending;
-//                    arg << argv[i + 2];
-//                    arg >> ending;
                     sscanf(argv[i + 1], "%d", &starting);
                     sscanf(argv[i + 2], "%d", &ending);
                     break;
                 case 't':
-//                    arg << argv[i + 1];
-//                    arg >> threads;
                     sscanf(argv[i + 1], "%d", &threads);
-//                    threads = strtol(argv[i + 1], &threads, 10);
                     break;
                 case 'h':
                     show_help();
                     break;
             }
-//            if (arg == "-r") {
-//                sscanf(argv[i + 1], "%d", &starting);
-//                sscanf(argv[i + 2], "%d", &ending);
-//            } else if (arg == "-t") {
-//                sscanf(argv[i + 1], "%d", &threads);
-//            }
         }
 
     }
@@ -79,21 +52,6 @@ int main(int argc, char **argv) {
     };
 
     vector<thread> ThreadVector;
-    /*
-    int input_start = 0;
-    int input_end = 0;
-    int end_val = 0;
-    int interval = (ending - starting) / threads;
-    for (int i = 0; i < threads; i++) {
-        input_start += end_val + starting;
-        input_end += end_val + interval;
-
-        if (i == threads - 1) input_end = ending;
-        cout << YELLOW << "RangePrime(" << input_start << ", " << input_end << ")" << RESET << endl;
-        ThreadVector.emplace_back(thread(testfn, input_start, input_end, i));
-
-        input_start = input_end;
-    }*/
     int input_start = starting;
     int input_end;
     int interval = (ending - starting) / threads;
@@ -122,10 +80,11 @@ int main(int argc, char **argv) {
         index++;
     }
 
-//    cout << endl << "Global variable storing number of primes: " << num_of_primes << endl;
-//    cout << endl << "Local variable storing number of primes: " << numeros_of_primes << endl;
+
+    auto ending_time = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration<double, milli>(ending_time - starting_time).count();
     cout << endl << endl << "Number of threads used: " << threads << endl;
     cout << "There are " << RED << numeros_of_primes << RESET << " primes between " << starting << " and " << ending << "." << endl;
-    cout << "elapsed time: " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << "ms" << endl;
+    cout << "elapsed time: " << duration << "ms" << endl;
     return 0;
 }
